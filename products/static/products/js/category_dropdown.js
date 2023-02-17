32163a77ac4204daab3838e4956b1cf67dd1c519
yasser <friha.yasser@pm.me>
2023-02-17 09:52:11 +0100

    removed unnecessary files and moved js code into individual files

---
+++ products/static/products/js/category_dropdown.js
@@ -0,0 +1,11 @@
+document.querySelector('.dropdown-toggle').addEventListener('click', function () {
+    document.querySelector('.dropdown-menu').classList.toggle('show');
+  });
+  document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('mouseover', function () {
+      this.querySelector('.child-menu').style.display = 'block';
+    });
+    item.addEventListener('mouseout', function () {
+      this.querySelector('.child-menu').style.display = 'none';
+    });
+  });
