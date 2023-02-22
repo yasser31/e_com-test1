15569c1e07661fbd413be5b9e3c37885264882fc
yasser <yassr31ph@gmail.com>
2023-02-22 23:17:50 +0100

    added ip address to allowed hosts

--- e__com/settings.py
+++ e__com/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "46.101.6.122/"]
 
 
 # Application definition
